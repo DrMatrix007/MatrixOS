cb927747463b97802e294a5c2440bdcec8ddc984
Ofri Haviv <haviv.ofri@gmail.com>
2025-07-06 00:00:13 +0300

    fix small problem in script. this is gonna be fun!

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 /usr/bin/cmake --preset=i686 -DCMAKE_INSTALL_PREFIX=out/install/i686 \
  -Bout/build/i686
 /usr/bin/cmake --build out/build/i686 --parallel 30
+mkdir -p ./bin
 cp out/build/i686/matrix_os ./bin/matrix_os

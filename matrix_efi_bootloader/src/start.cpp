43cec58a8cefbb2408dadeb520891938bcf830c1
Ofri Haviv <haviv.ofri@gmail.com>
2025-08-23 09:17:06 +0300

    added match_or macro

--- matrix_efi_bootloader/src/start.cpp
+++ matrix_efi_bootloader/src/start.cpp
@@ -19,29 +19,26 @@ extern "C" EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle,
 
     mst::optional<simple_output_protocol&> out_opt = table.out();
 
+    match_or(simple_out, out_opt, return -1);
+
+    simple_out.clear_screen();
+    simple_out.output_string((wchar_t*)L"hello world\n");
+
+    auto gop_opt = table.get_protocol<graphics_protocol>();
+
+    match_or(gop, gop_opt, return status);
+
+    auto res = gop.try_find_mode(1920, 1080);
+    match(index, res)
+    {
+        gop.set_mode(index);
+    }
+
     match(simple_out, out_opt)
     {
         simple_out.clear_screen();
         simple_out.output_string((wchar_t*)L"hello world\n");
     }
-
-    auto gop = table.get_protocol<graphics_protocol>();
-
-    match(gop, gop)
-    {
-        auto res = gop.try_find_mode(1920, 1080);
-        match(index, res)
-        {
-            gop.set_mode(index);
-        }
-    }
-
-    match(simple_out, out_opt)
-    {
-        simple_out.clear_screen();
-        simple_out.output_string((wchar_t*)L"hello world\n");
-    }
-
 
     while (true)
     {
--- standard_matrix/src/match.hpp
+++ standard_matrix/src/match.hpp
@@ -6,6 +6,9 @@
 
 #define _MATCH_CAT(a, b) a##b
 #define _MATCH_DISPATCH(N, ...) _MATCH_CAT(_MATCH_IMPL_, N)(__VA_ARGS__)
+#define _MATCH_CAT(a, b) a##b
+#define _MATCH_XCAT(a, b) _MATCH_CAT(a, b)
+#define _MATCH_UNIQUE(base) _MATCH_XCAT(base, __LINE__)
 
 /// use:
 /// match(type, var, viewable)
@@ -20,4 +23,23 @@
     if (auto&& _match_data = (DATA); !(_match_data.view()).is_empty())         \
         for (auto&& VAR : _match_data.view())
 
+#define _MATCH_OR_DISPATCH(N, ...) _MATCH_CAT(_MATCH_OR_IMPL_, N)(__VA_ARGS__)
+
+#define match_or(...) _MATCH_OR_DISPATCH(_MATCH_NARGS(__VA_ARGS__), __VA_ARGS__)
+
+#define _MATCH_OR_IMPL_4(TYPE, VAR, DATA, ON_EMPTY)                            \
+    auto&& _MATCH_UNIQUE(_match_or_data) = (DATA).view<TYPE>();                \
+    if (_MATCH_UNIQUE(_match_or_data).is_empty())                              \
+    {                                                                          \
+        return ([&]() { ON_EMPTY; })();                                        \
+    }                                                                          \
+    auto&& VAR = *(_MATCH_UNIQUE(_match_or_data).begin())
+
+#define _MATCH_OR_IMPL_3(VAR, DATA, ON_EMPTY)                                  \
+    auto&& _MATCH_UNIQUE(_match_or_data) = (DATA).view();                      \
+    if (_MATCH_UNIQUE(_match_or_data).is_empty())                              \
+    {                                                                          \
+        return ([&]() { ON_EMPTY; })();                                        \
+    }                                                                          \
+    auto&& VAR = *(_MATCH_UNIQUE(_match_or_data).begin())
 #endif // STANDARD_MATRIX_MATCH_H

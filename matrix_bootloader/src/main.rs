422cd9c1e49be5a6f6a6538867b71b4edab1a3e0
Ofri Haviv <haviv.ofri@gmail.com>
2026-01-08 22:19:17 +0200

    working basic gop

--- matrix_bootloader/src/main.rs
+++ matrix_bootloader/src/main.rs
@@ -2,13 +2,27 @@
 #![no_std]
 
 use log::info;
-use uefi::Status;
+use uefi::{
+    Status, boot,
+    proto::console::gop::{self, BltOp, BltPixel, GraphicsOutput},
+};
 
 #[uefi::entry]
 fn main() -> Status {
     uefi::helpers::init().unwrap();
 
     info!("Hello from Matrix bootloader!");
+
+    let gop_handle = boot::get_handle_for_protocol::<GraphicsOutput>().unwrap();
+    let mut gop = boot::open_protocol_exclusive::<GraphicsOutput>(gop_handle).unwrap();
+
+    let (x,y) = gop.current_mode_info().resolution();
+
+    gop.blt(BltOp::VideoFill {
+        color: BltPixel::new(0x66, 0x69, 0x69),
+        dest: (0, 0),
+        dims: (x, y),
+    }).unwrap();
 
     loop {
         unsafe { core::arch::asm!("hlt") }

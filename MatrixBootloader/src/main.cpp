7b65b8cd2980a31200aa9d05247e9e516d7af801
Ofri Haviv <haviv.ofri@gmail.com>
2026-01-16 22:14:32 +0200

    added a lib. not working yet.

--- MatrixBootloader/src/main.cpp
+++ MatrixBootloader/src/main.cpp
@@ -5,7 +5,18 @@ import MatrixEfiTable;
 
 extern "C" EFI_STATUS efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table)
 {
+    ST = system_table;
+
     mefi::MatrixEfiTable table;
+
+    EFI_INPUT_KEY key;
+
+    EFI_STATUS status = ST->ConIn->Reset(ST->ConIn, FALSE);
+    if (EFI_ERROR(status))
+        return status;
+
+
+    while ((status = ST->ConIn->ReadKeyStroke(ST->ConIn, &key)) == EFI_NOT_READY) {}
 
     return EFI_SUCCESS;
 }
---
+++ StandardMatrix/src/standard_test.ixx
@@ -0,0 +1,10 @@
+//
+// Created by ofrih on 1/16/26.
+//
+
+export module standard_test;
+
+export int add(const int x, const int y)
+{
+    return x + y;
+}

163e721d1e6190392cf137f74d210a953ff8b9cf
Ofri Haviv <haviv.ofri@gmail.com>
2026-01-16 09:16:17 +0200

    another damn big reset

--- matrix_bootloader_builder/src/lib.rs
+++
@@ -1,5 +0,0 @@
-static IMAGE: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/bootloader.img"));
-
-pub fn get_bootloader_image() -> &'static [u8] {
-    IMAGE
-}
--- matrix_kernel_builder/src/lib.rs
+++
@@ -1,5 +0,0 @@
-static KERNEL_BYTES: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/kernel.img"));
-
-pub fn get_kernel_image() -> &'static [u8] {
-    return KERNEL_BYTES;
-}
